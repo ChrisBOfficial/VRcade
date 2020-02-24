7715cb8f6689cc9503dda3ecbf584d2949e6d46c
VRCart01 <54551163+rinish06@users.noreply.github.com>
2020-02-24 17:03:16 -0600

    Added Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject head;
+
+    public float m_Sensitivity = 1.0f; // -1 to 1
+    public float m_MaxSpeed = 1.0f;
+
+    public SteamVR_Action_Boolean m_MovePress = null;
+    public SteamVR_Action_Vector2 m_MoveValue = null;
+
+    private float m_Speed = 0.0f;
+
+    private CharacterController m_CharacterController = null;
+
+    private void Awake()
+    {
+        Debug.Log("Started Movement Calculation Script");
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        // figure out movement orientation
+        Vector3 orientationEuler = new Vector3(0, head.transform.eulerAngles.y, 0);
+        Quaternion orientation = Quaternion.Euler(orientationEuler);
+        Vector3 movement = Vector3.zero;
+
+        // if not moving
+        if (m_MovePress.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            m_Speed = 0;
+        }
+
+        // if button pressed
+        if (m_MovePress.state)
+        {
+            Debug.Log(m_MoveValue.axis);
+            // add, clamp
+            m_Speed += m_MoveValue.axis.y * m_Sensitivity;
+            m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed); // can reduce backward speed here
+
+            // orientation - moving in the direction we are looking at
+            movement += orientation * (m_Speed * Vector3.forward) * Time.deltaTime;
+        }
+
+        // apply
+        m_CharacterController.Move(movement);
+    }
+}
