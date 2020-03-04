0318fbdaec17f34b180f9f135cb0bb1b851f80d2
iusk-VRLab <mystaraayush@gmail.com>
2020-03-04 16:26:52 -0600

    Added Waypoint system, Implementing different robot modes

---
+++ Assets/Scripts/RobotButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotButton : MonoBehaviour
+{
+    public RobotMovement robot;
+
+    void onTriggerEnter(Collider collider)
+    {
+        Debug.Log("Triggered");
+        if (collider.gameObject.CompareTag("Hand"))
+        {
+            robot.ChangeMode(true);
+        }
+    }
+}
