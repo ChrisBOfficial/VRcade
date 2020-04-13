c1c4c2e637fc554e0dd5ed43bdc54459ae31ed2b
Chris B <officialchrisb@icloud.com>
2020-04-13 17:21:11 -0500

    Adjusted belt speed and completed housing

--- Assets/Scripts/BeltTransfer.cs
+++ Assets/Scripts/BeltTransfer.cs
@@ -7,6 +7,7 @@ public class BeltTransfer : MonoBehaviour
     public Vector3 movePosition;
     private void OnCollisionEnter(Collision otherThing)
     {
-        otherThing.gameObject.transform.position = movePosition;
+        // otherThing.gameObject.transform.position = movePosition;
+        otherThing.gameObject.transform.position = new Vector3(otherThing.gameObject.transform.position.x + 14, otherThing.gameObject.transform.position.y, otherThing.gameObject.transform.position.z);
     }
 }
