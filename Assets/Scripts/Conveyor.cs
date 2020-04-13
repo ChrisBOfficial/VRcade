1890c476b279f0d83b0584e7c5c5b910296e76d2
Chris B <officialchrisb@icloud.com>
2020-04-13 14:46:06 -0500

    Rebuilt skeleton

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public float speed;
+    public float visualSpeedScalar;
+
+    private Vector3 direction;
+    private float currentScroll;
+
+    private void Update()
+    {
+        // Scroll texture to appear moving
+        currentScroll = currentScroll + Time.deltaTime * speed * -visualSpeedScalar;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(currentScroll, 0);
+    }
+
+    private void OnCollisionStay(Collision otherThing)
+    {
+        direction = -transform.right * speed;
+        otherThing.rigidbody.AddForce(direction, ForceMode.Acceleration);
+    }
+}
