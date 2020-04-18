e087aaac9932693f252870db76513e66ff05574d
khpolivanov <31524380+khpolivanov@users.noreply.github.com>
2020-04-18 13:59:10 -0500

    Merge changes

---
+++ Assets/Scripts/BeltTransfer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltTransfer : MonoBehaviour
+{
+    public Vector3 movePosition;
+    private void OnCollisionEnter(Collision otherThing)
+    {
+        // otherThing.gameObject.transform.position = movePosition;
+        otherThing.gameObject.transform.position = new Vector3(otherThing.gameObject.transform.position.x + 14, otherThing.gameObject.transform.position.y, otherThing.gameObject.transform.position.z);
+    }
+}
